793727a9c725a680698520d5ce1dcbbf9e9b3c7a
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2025-10-11 17:09:31 +0200

    refactor: reorganize code for more readability
    Remove useless 'modules' section in dune file (added when I first started, some months ago), add module aliases for CamlImages to distinguish them, and refactor some code

--- src/math.ml
+++ src/math.ml
@@ -4,20 +4,16 @@
   File: math.ml
 *)
 
-module Math = struct
-  (*
-    Calculates the euclidean distance between two 3d integer tuples.
-  *)
-  let euclidean_distance a b: float =
-    let (x1, y1, z1) = a in
-    let (x2, y2, z2) = b in
-    let dx = float_of_int (x2 - x1) in
-    let dy = float_of_int (y2 - y1) in
-    let dz = float_of_int(z2 - z1) in
-    sqrt( (dx *. dx) +. (dy *. dy) +. (dz *. dz) )
+(* Calculates the euclidean distance between two 3d integer tuples. *)
+let euclidean_distance a b: float =
+  let (x1, y1, z1) = a in
+  let (x2, y2, z2) = b in
+  let dx = float_of_int (x2 - x1) in
+  let dy = float_of_int (y2 - y1) in
+  let dz = float_of_int(z2 - z1) in
+  sqrt( (dx *. dx) +. (dy *. dy) +. (dz *. dz) )
 
 
-  (* Returns the euclidean distance between two RGB colors as (r,g,b) tuples. *)
-  let color_distance (r1, g1, b1) (r2, g2, b2) =
-      euclidean_distance (r1, g1, b1) (r2, g2, b2)
-end
+(* Returns the euclidean distance between two RGB colors as (r,g,b) tuples. *)
+let color_distance (r1, g1, b1) (r2, g2, b2) =
+    euclidean_distance (r1, g1, b1) (r2, g2, b2)
